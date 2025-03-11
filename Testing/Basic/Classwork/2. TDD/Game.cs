336153134a54bed53e98ecb3cd4c8d9d61bfc73e
DaniilUskov <90408997+DaniilUskov@users.noreply.github.com>
2025-03-11 17:45:37 +0500

    Done ClassWork Tasks

--- Testing/Basic/Classwork/2. TDD/Game.cs
+++ Testing/Basic/Classwork/2. TDD/Game.cs
@@ -1,14 +1,99 @@
 ﻿namespace TDD.Task;
 public class Game
 {
+    private int FramesInGame { get; set; } = 10;
+    
+    private int FramesLeft { get; set; } = 10;
+
+    private List<(int FirstRoll, int SecondRoll)> Rolls { get; set; } =
+    [
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1),
+        (FirstRoll: -1, SecondRoll: -1)
+    ];
+
+    private void IncreaseFramesInGameAfterStrikeOrSpare()
+    {
+        FramesInGame += 1;
+        FramesLeft += 1;
+    }
+
     public void Roll(int pins)
     {
-        throw new NotImplementedException();
+        var index = FramesInGame - FramesLeft;
+        if (index > FramesInGame - 1)
+        {
+            throw new Exception("Game is over!");
+        }
+        
+        var currentRoll = Rolls[index];
+
+        if (currentRoll.FirstRoll == -1)
+        {
+            currentRoll.FirstRoll = pins;
+            if (pins == 10)
+            {
+                currentRoll.SecondRoll = 0;
+                FramesLeft--;
+            }
+        }
+        else
+        {
+            currentRoll.SecondRoll = pins;
+            FramesLeft--;
+        }
+        
+        Rolls[index] = currentRoll;
+
+        if (index == FramesInGame - 1 
+            && currentRoll.FirstRoll + currentRoll.SecondRoll == 10
+            && FramesInGame == 10)
+        {
+            IncreaseFramesInGameAfterStrikeOrSpare();
+        }
     }
 
     public int GetScore()
     {
-        throw new NotImplementedException();
+        var score = 0;
+        var isSpare = false;
+        var isStrike = false;
+        for (var i = 0; i < FramesInGame - FramesLeft; i++)
+        {
+            if (isStrike)
+            {
+                score += Rolls[i].FirstRoll * 2 + Rolls[i].SecondRoll * 2;
+                isStrike = false;
+            }
+            else if (isSpare)
+            {
+                score += Rolls[i].FirstRoll * 2 + Rolls[i].SecondRoll;
+                isSpare = false;
+            }
+            else
+            {
+                score += Rolls[i].FirstRoll + Rolls[i].SecondRoll;
+            }
+            
+            if (Rolls[i].FirstRoll == 10 || Rolls[i].SecondRoll == 10)
+            {
+                isStrike = true;
+            }
+            else if (Rolls[i].FirstRoll + Rolls[i].SecondRoll == 10)
+            {
+                isSpare = true;
+            }
+        }
+
+        return score;
     }
 }
 
--- Testing/Basic/Classwork/2. TDD/GameTests.cs
+++ Testing/Basic/Classwork/2. TDD/GameTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Reflection;
+using FluentAssertions;
 using NUnit.Framework;
 using TDD.Task;
 
@@ -14,5 +15,156 @@ public class GameTests
         new Game()
             .GetScore()
             .Should().Be(0);
+    }
+
+    [Test]
+    public void LeftFramesAreTen_BeforeAnyRolls()
+    {
+        var game = new Game();
+        var framesLeft = game
+            .GetType()
+            .GetProperties(BindingFlags.NonPublic | BindingFlags.Instance)
+            .First(x => x.Name == "FramesLeft");
+        
+        framesLeft.GetValue(game).Should().Be(10);
+    }
+
+    [Test]
+    public void LeftFramesProperty_IsPrivate()
+    {
+        var game = new Game();
+        var privateProperties = game
+            .GetType()
+            .GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+        
+        privateProperties.Should().Contain(x => x.Name == "FramesLeft");
+    }
+
+    [Test]
+    public void DecreaseFramesLeft_AfterRoll()
+    {
+        var game = new Game();
+        var framesLeft = game
+            .GetType()
+            .GetProperties(BindingFlags.NonPublic | BindingFlags.Instance)
+            .First(x => x.Name == "FramesLeft");
+        
+        game.Roll(0);
+        game.Roll(1);
+        
+        framesLeft.GetValue(game).Should().Be(9);
+    }
+
+    [Test]
+    public void GetScore_ReturnsZero_BeforeAnyRolls()
+    {
+        new Game()
+            .GetScore()
+            .Should().Be(0);
+    }
+
+    [Test]
+    public void CorrectScore_AfterSimpleFrame()
+    {
+        var game = new Game();
+        game.Roll(5);
+        game.Roll(2);
+        
+        game.GetScore().Should().Be(7);
+    }
+
+    [Test]
+    public void CorrectScore_AfterSpareFrame()
+    {
+        var game = new Game();
+        game.Roll(5);
+        game.Roll(5);
+        game.Roll(2);
+        game.Roll(1);
+        
+        game.GetScore().Should().Be(15);
+    }
+
+    [Test]
+    public void CorrectScore_AfterStrikeFrame()
+    {
+        var game = new Game();
+        game.Roll(10);
+        game.Roll(5);
+        game.Roll(2);
+        
+        game.GetScore().Should().Be(24);
+    }
+
+    [Test]
+    public void CorrectScore_AfterMultipleFrames()
+    {
+        var game = new Game();
+        game.Roll(10);
+        game.Roll(5);
+        game.Roll(2);
+        game.Roll(1);
+        game.Roll(1);
+        
+        game.GetScore().Should().Be(26);
+    }
+
+    [Test]
+    public void CorrectScore_AfterStrikeFrameOnLastFrame()
+    {
+        var game = new Game();
+
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(10);
+        game.Roll(10);
+        
+        game.GetScore().Should().Be(102);
+    }
+
+    [Test]
+    public void CantRoll_AfterTenFramesWithoutStrikeOrSpare()
+    {
+        var game = new Game();
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        game.Roll(8);
+        game.Roll(0);
+        
+        var action = new Action(() => game.Roll(1));
+        
+        action.Should().Throw<Exception>().WithMessage("Game is over!");
     }
 }
