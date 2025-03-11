16be4db0d9794f8a9c25571f4aa7e3ef4e1aa2e4
DaniilUskov <90408997+DaniilUskov@users.noreply.github.com>
2025-03-11 12:47:27 +0500

    Added WordsStatistics test

--- Testing/Basic/Classwork/1. WordsStatistics/WordsStatisticsTests.cs
+++ Testing/Basic/Classwork/1. WordsStatistics/WordsStatisticsTests.cs
@@ -25,6 +25,7 @@ public class WordsStatisticsTests
         return new WordsStatisticsImpl();
     }
 
+    #region -- GetStatistics
 
     [Test]
     public void GetStatistics_IsEmpty_AfterCreation()
@@ -46,4 +47,76 @@ public class WordsStatisticsTests
         wordsStatistics.AddWord("def");
         wordsStatistics.GetStatistics().Should().HaveCount(2);
     }
+
+    [Test]
+    public void GetStatistics_ItemsAreOrderedByCountInDescendingOrder_AfterAdditionOfDifferentWords()
+    {
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.AddWord("defg");
+        wordsStatistics.AddWord("defg");
+        wordsStatistics.GetStatistics().Should().Equal(new WordCount("defg", 2), new WordCount("abc", 1));
+    }
+    
+    [Test]
+    public void GetStatistics_ItemsAreOrderedLexicographicInDescendingOrder_AfterAdditionOfDifferentWords()
+    {
+        wordsStatistics.AddWord("defg");
+        wordsStatistics.AddWord("defg");
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.GetStatistics().Should().Equal(new WordCount("abc", 2), new WordCount("defg", 2));
+    }
+
+    #endregion
+
+    #region -- AddWord
+
+    [Test]
+    public void AddWord_ThrowsArgumentNullException_WhenWordIsNull()
+    {
+        var act = () => wordsStatistics.AddWord(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void AddWord_DoesNotAddWord_WhenWordIsEmpty()
+    {
+        wordsStatistics.AddWord(string.Empty);
+        wordsStatistics.GetStatistics().Should().BeEmpty();
+    }
+
+    [Test]
+    public void AddWord_DoesNotAddWord_WhenWordIsWhiteSpace()
+    {
+        wordsStatistics.AddWord(" ");
+        wordsStatistics.GetStatistics().Should().BeEmpty();
+    }
+
+    [Test]
+    public void AddWord_CountWordsCorrect_AfterAdditionOfSameWords()
+    {
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.GetStatistics().Should().Equal(new WordCount("abc", 3));
+    }
+
+    [Test]
+    public void AddWord_CountWordsCorrect_AfterAdditionSameWordsWithDifferentCases()
+    {
+        wordsStatistics.AddWord("abc");
+        wordsStatistics.AddWord("ABC");
+        wordsStatistics.AddWord("Abc");
+        wordsStatistics.GetStatistics().Should().Equal(new WordCount("abc", 3));
+    }
+
+    [Test]
+    public void AddWord_DoesNotAddFullWord_WhenWordsLengthIsGreaterThan10()
+    {
+        wordsStatistics.AddWord("abcaaaaaaaaaaaaaaaaaaaaaaa");
+        wordsStatistics.GetStatistics().Should().Equal(new WordCount("abcaaaaaaa", 1));
+    }
+
+    #endregion
 }
