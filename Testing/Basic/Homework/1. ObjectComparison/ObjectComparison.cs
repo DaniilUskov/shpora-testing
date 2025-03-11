3e13c2bcad07f0050d37a6720847f088e0e5545a
DaniilUskov <90408997+DaniilUskov@users.noreply.github.com>
2025-03-11 18:31:34 +0500

    Solved homework

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
@@ -20,11 +21,82 @@ public class ObjectComparison
         ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
         ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
 
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
+        ClassicAssert.AreEqual(expectedTsar.Parent.Name, actualTsar.Parent.Name);
         ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
         ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
         ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
     }
+
+    #region -- Simple Tsar without parents tests
+
+    [Test]
+    [TestCase("Ivan IV The Terrible")]
+    public void TsarName_IsCorrect_AfterInitialization(string expectedTsarName)
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+
+        actualTsar.Name.Should().BeEquivalentTo(expectedTsarName);
+    }
+
+    [Test]
+    [TestCase(54)]
+    public void TsarAge_IsCorrect_AfterInitialization(int expectedTsarAge)
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+
+        actualTsar.Age.Should().Be(expectedTsarAge);
+    }
+
+    [Test]
+    [TestCase(170)]
+    public void TsarHeight_IsCorrect_AfterInitialization(int expectedTsarHeight)
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+
+        actualTsar.Height.Should().Be(expectedTsarHeight);
+    }
+
+    [Test]
+    [TestCase(70)]
+    public void TsarWeight_IsCorrect_AfterInitialization(int expectedTsarWeight)
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+
+        actualTsar.Weight.Should().Be(expectedTsarWeight);
+    }
+
+    #endregion
+
+    #region -- Tsar with parents tests
+
+    [Test]
+    public void TsarParents_IsCorrect_AfterInitialization()
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+        var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+            new Person("Vasili III of Russia", 28, 170, 60, null));
+
+        actualTsar.Parent
+            .Should()
+            .BeEquivalentTo(expectedTsar.Parent, options => options.IncludingNestedObjects().Excluding(x => x.Id));
+    }
+
+    #endregion
+    
+    /*
+     * Подход с Fluent Assertions лучше, потому что в случае с CheckCurrentTsar_WithCustomEquality при добавлении
+     * новых свойств в классе Person нам пришлось бы добавлять новые условия сравнения в метод AreEqual, а в случае с
+     * Fluent Assertions достаточно будет добавить их в тест, а сравнение будет происходить автоматически.
+     *
+     * В Fluent Assertions можно также указывать какие свойства не нужно сравнивать с помощью Fluent-api интерфейса.
+     *
+     * Также сильно повысится читаемость кода, из-за того же Fluent-api интерфейса, так как цепочку методов мы можем
+     * читать как обычное предложение на английском языке.
+     *
+     * Также в случае с CheckCurrentTsar_WithCustomEquality мы по взгляду на тест не понимаем как именно мы сравниваем,
+     * чтобы понять нужно перейти в метод AreEqual, а если у нас свойств будет много, то нам и код AreEqual будет разрастаться,
+     * что приведет к трудностям в чтении кода.
+     */
 
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -8,6 +9,7 @@ namespace HomeExercise.Tasks.NumberValidator;
 public class NumberValidatorTests
 {
     [Test]
+    [Description("Старый тест")]
     public void Test()
     {
         Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
@@ -17,7 +19,6 @@ public class NumberValidatorTests
 
         ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
         ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
         ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
@@ -28,4 +29,109 @@ public class NumberValidatorTests
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
     }
+
+    #region -- Constructor tests
+
+    [Test]
+    public void Ctor_ShouldThrowArgumentException_WhenPrecisionIsLessThanZero()
+    {
+        var act = () => new NumberValidator(-1, 1, true);
+        
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Ctor_ShouldGiveCorrectErrorMessage_WhenPrecisionIsLessThanZero()
+    {
+        var act = () => new NumberValidator(-1, 1, true);
+        
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a positive number");
+    }
+
+    [Test]
+    public void Ctor_ShouldThrowArgumentException_WhenScaleIsLessThanZero()
+    {
+        var act = () => new NumberValidator(1, -1, true);
+        
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Ctor_ShouldThrowArgumentException_WhenScaleIsMoreThanPrecision()
+    {
+        var act = () => new NumberValidator(1, 2, true);
+        
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Ctor_ShouldGiveCorrectErrorMessage_WhenScaleIsLessThanZeroOrMoreThanPrecision()
+    {
+        var act = () => new NumberValidator(1, 2, true);
+        
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a non-negative number less or equal than precision");
+    }
+
+    #endregion
+
+    #region -- IsValidNumber tests
+
+    [Test]
+    public void IsValidNumber_ShouldReturnFalse_WhenInputIsNull()
+    {
+        var validator = new NumberValidator(3, 2, true);
+        
+        validator.IsValidNumber(null!).Should().BeFalse();
+    }
+    
+    [Test]
+    public void IsValidNumber_ShouldReturnFalse_WhenInputIsEmpty()
+    {
+        var validator = new NumberValidator(3, 2, true);
+        
+        validator.IsValidNumber(string.Empty).Should().BeFalse();
+    }
+
+    [Test]
+    [TestCase("abc")]
+    [TestCase("a.bc")]
+    public void IsValidNumber_ShouldReturnFalse_WhenInputIsNotNumber(string input)
+    {
+        var validator = new NumberValidator(3, 2, true);
+        
+        validator.IsValidNumber(input).Should().BeFalse();
+    }
+
+    [Test]
+    [TestCase("0.0")]
+    [TestCase("+1.23")]
+    public void IsValidNumber_ShouldReturnTrue_WhenInputIsValid(string input)
+    {
+        var validator = new NumberValidator(4, 2, true);
+        
+        validator.IsValidNumber(input).Should().BeTrue();
+    }
+
+    [Test]
+    [TestCase("0.000")]
+    [TestCase("-1.23")]
+    [TestCase("+1.23")]
+    public void IsValidNumber_ShouldReturnFalse_WhenInputIsNotValid(string input)
+    {
+        var validator = new NumberValidator(3, 2, true);
+        
+        validator.IsValidNumber(input).Should().BeFalse();
+    }
+
+    [Test]
+    public void IsValidNumber_ShouldReturnFalse_WhenOnlyPositiveIsTrueAndInputIsNegative()
+    {
+        var validator = new NumberValidator(3, 2, true);
+        
+        validator.IsValidNumber("-1.23").Should().BeFalse();
+    }
+
+    #endregion
 }
